b7a8bba9ea894a2e8c151cb539b7dd816685a90f
naturalnayasmetanka <marksmentina@gmail.com>
2026-05-10 01:03:21 +0300

    Add DepartmentLocation domain

---
+++ ds/src/DS.Domain/DepartmentsLocations/DepartmentLocation.cs
@@ -0,0 +1,20 @@
+﻿namespace DS.Domain.DepartmentsLocations;
+
+public class DepartmentLocation
+{
+    private DepartmentLocation() { }
+
+    public Guid Id { get; private set; }
+    public Guid DepartmentId { get; private set; }
+    public Guid LocationId { get; private set; }
+
+    public DepartmentLocation(
+       Guid id,
+       Guid departmentId,
+       Guid locationId)
+    {
+        Id = id;
+        DepartmentId = departmentId;
+        LocationId = locationId;
+    }
+}
