bf6e9cfc8513c1dcad00303ff99dcb12a80680c7
naturalnayasmetanka <marksmentina@gmail.com>
2026-05-10 01:02:09 +0300

    Add Department domain

---
+++ ds/src/DS.Domain/Departments/Identifier.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DS.Domain.Departments;
+
+public record Identifier
+{
+    private Identifier(string Value)
+    {
+        this.Value = Value.Trim();
+    }
+
+    public string Value { get; }
+
+    public static Result<Identifier, string> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return Result.Failure<Identifier, string>
+                ($"{nameof(Identifier)} empty or null");
+        }
+
+        return Result.Success<Identifier, string>
+            (new Identifier(Value: value));
+    }
+}
