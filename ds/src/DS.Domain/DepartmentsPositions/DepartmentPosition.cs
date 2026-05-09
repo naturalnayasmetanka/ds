104f087907007d0e80f63e3c2cd6bdec37684626
naturalnayasmetanka <marksmentina@gmail.com>
2026-05-10 01:03:33 +0300

    Add DepartmentPosition domain

---
+++ ds/src/DS.Domain/DepartmentsPositions/DepartmentPosition.cs
@@ -0,0 +1,20 @@
+﻿  namespace DS.Domain.DepartmentsPositions;
+
+public class DepartmentPosition
+{
+    private DepartmentPosition() { }
+
+    public Guid Id { get; private set; }
+    public Guid DepartmentId { get; private set; }
+    public Guid PositionId { get; private set; }
+
+    public DepartmentPosition(
+        Guid id,
+        Guid departmentId,
+        Guid positionId)
+    {
+        Id = id;
+        DepartmentId = departmentId;
+        PositionId = positionId;
+    }
+}
