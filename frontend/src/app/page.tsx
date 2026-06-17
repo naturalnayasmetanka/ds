049261e5dad3cc0833c2247c608f261bd0dd4dd4
naturalnayasmetanka <marksmentina@gmail.com>
2026-06-17 21:09:49 +0300

    Add base pages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
-import Playground from "./playground";
+import Locations from "./locations/page";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Home page",
+};
 
 export default function Home() {
-  return (
-    <Playground/>
-  );
+  return <Locations />;
 }
